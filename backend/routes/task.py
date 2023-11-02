c8469a3836d7b01b7d125da1c1435e2fb0c3b8d2
Mark-kus <tignanellimarco@gmail.com>
2023-11-02 12:33:06 -0300

    add lintin

--- backend/routes/task.py
+++ backend/routes/task.py
@@ -2,51 +2,59 @@ from fastapi import APIRouter, HTTPException
 from models import Task
 from schemas import UpdateTask
 from db import \
-get_many_tasks, \
-create_one_task, \
-get_one_task_by_title, \
-update_one_task, \
-delete_one_task
+    get_many_tasks, \
+    create_one_task, \
+    get_one_task_by_title, \
+    update_one_task, \
+    delete_one_task
 
 task = APIRouter(
-    prefix="/task"
+    prefix="/tasks",
+    tags=["Tasks"]
 )
 
-@task.get("/get")
-async def get_tasks():
-    return await get_many_tasks()
 
-@task.get("/get/{title}", response_model=Task)
+@task.get("/")
+async def get_tasks():
+    tasks = await get_many_tasks()
+    return {"data": tasks}
+
+
+@task.get("/{title}", response_model=Task)
 async def get_task(title: str):
     task = await get_one_task_by_title(title)
-    if task:
-        return task
-    raise HTTPException(404, f"Tarea con title {title} no encontrada")
+    if task is None:
+        raise HTTPException(404, f"Tarea con title {title} no encontrada")
+    return {"data": task}
 
-@task.post("/create/", response_model=Task, status_code=201)
-async def post_task(task: Task):    
+
+@task.post("/", response_model=Task, status_code=201)
+async def post_task(task: Task):
 
     taskFound = await get_one_task_by_title(task.title)
     if taskFound:
         raise HTTPException(409, "Ya existe")
 
-    response = await create_one_task(task.model_dump())
-    if response:
-        return response
-    raise HTTPException(400, "Algo salió mal")
+    created_task = await create_one_task(task.model_dump())  # type: ignore
+    if created_task is None:
+        raise HTTPException(400, "Algo salió mal")
+    return {"data": created_task}
 
-@task.put("/update/{title}")
+
+@task.put("/{title}")
 async def update_task(title: str, task: UpdateTask):
     task_existing = await get_one_task_by_title(title)
     print(task_existing)
     if task_existing is None:
-        raise HTTPException(404, "No existe")
-    return await update_one_task(title, task)
+        raise HTTPException(410, "No existe")
+    updated_task = await update_one_task(title, task)
+    return {"data": update_task}
 
-@task.delete("/delete/{title}")
+
+@task.delete("/{title}")
 async def delete_task(title: str):
     task = await get_one_task_by_title(title)
     if task is None:
-        raise HTTPException(404, "No existe")
+        raise HTTPException(410, "No existe")
     await delete_one_task(title)
-    return { "detail": "OK" }
+    return {"detail": "OK"}
